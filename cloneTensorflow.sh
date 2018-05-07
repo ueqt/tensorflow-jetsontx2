eaad5af6505774da2b88664cdad96f0550128e7e
ueqt <ueqtxu@gmail.com>
2018-05-07 18:03:21 +0800

    fix python3

--- cloneTensorflow.sh
+++ cloneTensorflow.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
-sudo apt-get install python-numpy python-dev python-pip python-wheel
+sudo apt-get install python3-numpy python3-dev python3-pip python3-wheel
 
 git clone https://github.com/tensorflow/tensorflow.git
 cp tensorflow1.8.patch tensorflow

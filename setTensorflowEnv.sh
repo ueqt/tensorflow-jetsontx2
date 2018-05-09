a1b205a2af6f0ddd23ef783ed34eca728fbb4003
ueqt <ueqtxu@gmail.com>
2018-05-09 10:55:50 +0800

    remove tensorrt

--- setTensorflowEnv.sh
+++ setTensorflowEnv.sh
@@ -47,9 +47,10 @@ export TF_NEED_OPENCL_SYCL=0
 # Use nvcc for CUDA compiler
 export TF_CUDA_CLANG=0
 # tensor rt
-export TF_NEED_TENSORRT=1
-export TF_TENSORRT_VERSION="3.0.2-1"
-export TENSORRT_INSTALL_PATH="/usr/lib/aarch64-linux-gnu"
+export TF_NEED_TENSORRT=0
+#export TF_NEED_TENSORRT=1
+#export TF_TENSORRT_VERSION="3.0.2-1"
+#export TENSORRT_INSTALL_PATH="/usr/lib/aarch64-linux-gnu"
 export TF_SET_ANDROID_WORKSPACE=0
 export TF_NCCL_VERSION="1"
 

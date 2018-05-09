6d549e45a15a26352cb1bf657dea6f3f35f6ac4a
ueqt <ueqtxu@gmail.com>
2018-05-09 10:40:24 +0800

    improve build

--- buildTensorflow.sh
+++ buildTensorflow.sh
@@ -1,14 +1,7 @@
 sudo cp /usr/local/cuda-8.0/nvvm/libdevice/libdevice.compute_50.10.bc /usr/local/cuda-8.0/nvvm/libdevice/libdevice.10.bc
 sudo cp /usr/local/cuda-8.0/nvvm/libdevice/libdevice.10.bc /usr/local/cuda-8.0/libdevice.10.bc
 
-export TF_NEED_CUDA=1
-export TF_CUDA_VERSION=9.0
-export CUDA_TOOLKIT_PATH=/usr/local/cuda
-export TF_CUDNN_VERSION=7
-export CUDNN_INSTALL_PATH=/usr/lib/aarch64-linux-gnu/
-export TF_CUDA_COMPUTE_CAPABILITIES=6.2
-
 cd tensorflow
 
-bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel build --config=opt --verbose_failures --config=cuda //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
